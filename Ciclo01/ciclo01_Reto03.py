85480ed48d57986e169c4c8a245944033a661b6d
Daniel Caldas <zthebancaldas@gmail.com>
2021-06-14 22:54:53 -0500

    main

---
+++ Ciclo01/ciclo01_Reto03.py
@@ -0,0 +1,27 @@
+LetrasCarlos:str
+LetrasValeria:str
+Pekkemons:str
+
+def juegoPekkemon (LetrasCarlos:str, LetrasValeria:str, Pekkemons:str):
+    longitudCarlos = (int(len(Pekkemons)/len(LetrasCarlos))*LetrasCarlos)+LetrasCarlos[0:2]
+    longitudValeria = (int(len(Pekkemons)/len(LetrasValeria))*LetrasValeria)+LetrasValeria[0:2]
+    list(longitudCarlos)
+    list(longitudValeria)
+    pekkedex = list(Pekkemons)
+    list(pekkedex)
+    for i in pekkedex:
+        if longitudCarlos == pekkedex and longitudValeria == pekkedex:
+            puntosCarlos = puntosCarlos+1
+            puntosValeria = puntosValeria+1
+            print('P')
+        if longitudCarlos== pekkedex and longitudValeria != pekkedex:
+            puntosCarlos = puntosCarlos+1
+            print('C')
+        if longitudCarlos != pekkedex and longitudValeria == pekkedex:
+            puntosValeria = puntosValeria+1
+            print('V')
+        if longitudCarlos != pekkedex and longitudValeria != pekkedex:
+            print('N')
+
+
+print(juegoPekkemon('AYS','LQK','YSGLZQUQSKRQSBVLVJGQ'))
