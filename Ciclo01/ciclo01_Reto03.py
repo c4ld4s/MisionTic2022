09f2474695145ec231db890713376d6bf29671e8
Daniel Caldas <zthebancaldas@gmail.com>
2021-06-16 03:39:44 -0500

    main

--- Ciclo01/ciclo01_Reto03.py
+++ Ciclo01/ciclo01_Reto03.py
@@ -3,35 +3,27 @@ LetrasValeria:str
 Pekkemons:str
 
 def juegoPekkemon (LetrasCarlos:str, LetrasValeria:str, Pekkemons:str):
-    longitudCarlos = (int(len(Pekkemons)/len(LetrasCarlos))*LetrasCarlos)+LetrasCarlos[0:2]
-    longitudValeria = (int(len(Pekkemons)/len(LetrasValeria))*LetrasValeria)+LetrasValeria[0:2]
-    print(list(longitudCarlos))
-    print(list(longitudValeria))
-    pekkedex = list(Pekkemons)
-    print(pekkedex)        
-    for i in range(0,len(pekkedex)):
+    print(LetrasCarlos)
+    print(LetrasValeria)
+    print(Pekkemons)
+    for i in Pekkemons:
         puntosCarlos = 0
         puntosValeria = 0
         lista = []
-        if longitudCarlos == pekkedex and longitudValeria == pekkedex:
+        if i in LetrasCarlos and i in LetrasValeria:
             puntosCarlos = puntosCarlos+1
             puntosValeria = puntosValeria+1
-            print('P')
-            i = i+1
-            return lista.append(['P'])
-        if longitudCarlos== pekkedex and longitudValeria!= pekkedex:
-            puntosCarlos = puntosCarlos+1
-            i = i+1
-            print('C')
-            return lista.append(['C'])
-        if longitudCarlos != pekkedex and longitudValeria == pekkedex:
+            lista.append(['P'])
+            print(list)
+        elif i not in LetrasCarlos and i in LetrasValeria:
             puntosValeria = puntosValeria+1
-            print('V')
-            i = i+1
-            return lista.append(['V'])
-        if longitudCarlos != pekkedex and longitudValeria != pekkedex:
-            print('N')
-            i = i+1
-            return lista.append(['N'])
+            lista.append(['V'])
+            print(list)
+        elif i  in LetrasCarlos and i not in LetrasValeria:
+            puntosValeria = puntosValeria+1
+            lista.append(['V'])
+            print(list)         
+        else:
+            print("error")
 
 print(juegoPekkemon('AYS','LQK','YSGLZQUQSKRQSBVLVJGQ'))
