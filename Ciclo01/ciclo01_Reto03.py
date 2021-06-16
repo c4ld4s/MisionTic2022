4ed6906dcf007cd8cb0e1c7f7252285d600ed27d
Daniel Caldas <zthebancaldas@gmail.com>
2021-06-16 04:38:06 -0500

    main

--- Ciclo01/ciclo01_Reto03.py
+++ Ciclo01/ciclo01_Reto03.py
@@ -6,24 +6,38 @@ def juegoPekkemon (LetrasCarlos:str, LetrasValeria:str, Pekkemons:str):
     print(LetrasCarlos)
     print(LetrasValeria)
     print(Pekkemons)
+    lista = []
+    puntosCarlos = 0
+    puntosValeria = 0
     for i in Pekkemons:
-        puntosCarlos = 0
-        puntosValeria = 0
-        lista = []
         if i in LetrasCarlos and i in LetrasValeria:
-            puntosCarlos = puntosCarlos+1
-            puntosValeria = puntosValeria+1
-            lista.append(['P'])
-            print(list)
+            puntosCarlos = puntosCarlos + 1
+            puntosValeria = puntosValeria + 1
+            lista.append('P')
+            print(lista)
+            print(str(puntosValeria) + "valeria")
+            print(str(puntosCarlos) + "carlos")
+            continue
         elif i not in LetrasCarlos and i in LetrasValeria:
-            puntosValeria = puntosValeria+1
-            lista.append(['V'])
-            print(list)
-        elif i  in LetrasCarlos and i not in LetrasValeria:
-            puntosValeria = puntosValeria+1
-            lista.append(['V'])
-            print(list)         
-        else:
-            print("error")
+            puntosValeria = puntosValeria + 1
+            lista.append('V')
+            print(lista)
+            print(str(puntosValeria) + "valeria")
+            print(str(puntosCarlos) + "carlos")
+            continue
+        elif i in LetrasCarlos and i not in LetrasValeria:
+            puntosCarlos = puntosCarlos + 1
+            lista.append('C')
+            print(lista)
+            print(str(puntosValeria) + "valeria")
+            print(str(puntosCarlos) + "carlos")
+            continue
+        elif i not in LetrasCarlos and i not in LetrasValeria:
+            puntosValeria = puntosValeria + 1
+            lista.append('P')
+            print(lista)
+            print(str(puntosValeria) + "valeria")
+            print(str(puntosCarlos) + "carlos")
+            continue
 
 print(juegoPekkemon('AYS','LQK','YSGLZQUQSKRQSBVLVJGQ'))
