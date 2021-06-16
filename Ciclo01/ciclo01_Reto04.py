50cbe364042f56fc81652958cc3ec42ca1c5ad50
Daniel Caldas <zthebancaldas@gmail.com>
2021-06-16 01:23:55 -0500

    main

--- Ciclo01/ciclo01_Reto04.py
+++ Ciclo01/ciclo01_Reto04.py
@@ -1,3 +1,18 @@
+#este es el diccionario que se usa para sacar los valores de la cuota del sindicato
+Valores_sindicatos = {
+    'sindicato0': 0,
+    'sindicato1': 13550,
+    'sindicato2': 18700,
+}
+
+#“””ValoresSindicales funcion que me devuelve la o las cuotas del sindicato que debo pagar, esta funcion se llamara dentro de la funcion calcularSalario y retornara una lista con los valores de las cuotas sindicales a pagar””” 
+def ValoresSindicales (llave, valor, datos=Valores_sindicatos):
+    return
+
+#“””Calcular salario
+def calcularSalario (empleados):
+    return
+
 empleados = [
     {
         'nombre' : "Juan David",
@@ -30,3 +45,5 @@ empleados = [
         }
     }
 ]
+
+calcularSalario(empleados)
