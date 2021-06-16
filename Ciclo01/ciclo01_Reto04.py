fa39e36c10980b950cbbcc3f513e4d77fde058d4
Daniel Caldas <zthebancaldas@gmail.com>
2021-06-16 01:17:01 -0500

    main

---
+++ Ciclo01/ciclo01_Reto04.py
@@ -0,0 +1,32 @@
+empleados = [
+    {
+        'nombre' : "Juan David",
+        'tipoContrato' : 'TERMINOFIJO',
+        'cargo' : 3,
+        'sindicatos' : {
+            'sindicato0' : False,
+            'sindicato1' : True,
+            'sindicato2' : False
+        }
+    },
+    {
+        'nombre' : "Juan David",
+        'tipoContrato' : 'TERMINOFIJO',
+        'cargo' : 3,
+        'sindicatos' : {
+            'sindicato0' : False,
+            'sindicato1' : True,
+            'sindicato2' : False
+        }
+    },
+        {
+        'nombre' : "Juan David",
+        'tipoContrato' : 'TERMINOFIJO',
+        'cargo' : 3,
+        'sindicatos' : {
+            'sindicato0' : False,
+            'sindicato1' : True,
+            'sindicato2' : False
+        }
+    }
+]
