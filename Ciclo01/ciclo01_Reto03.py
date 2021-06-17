440a68d12e6df28c3bf4d8749f0fef6865ff503a
Daniel Caldas <zthebancaldas@gmail.com>
2021-06-17 01:45:52 -0500

    main

--- Ciclo01/ciclo01_Reto03.py
+++ Ciclo01/ciclo01_Reto03.py
@@ -1,43 +1,30 @@
+#OBJETIVO= Definir una función que consiste en quien adivina la primera letra del nombre de los pekkémons que vienen en el camino.
+
 LetrasCarlos:str
 LetrasValeria:str
 Pekkemons:str
 
 def juegoPekkemon (LetrasCarlos:str, LetrasValeria:str, Pekkemons:str):
-    print(LetrasCarlos)
-    print(LetrasValeria)
-    print(Pekkemons)
-    lista = []
+    LetrasCarlos = list(LetrasCarlos)
+    LetrasValeria = list(LetrasValeria)
+    Pekkemons = list(Pekkemons)
+    pekkedex = []
     puntosCarlos = 0
     puntosValeria = 0
     for i in Pekkemons:
         if i in LetrasCarlos and i in LetrasValeria:
             puntosCarlos = puntosCarlos + 1
             puntosValeria = puntosValeria + 1
-            lista.append('P')
-            print(lista)
-            print(str(puntosValeria) + "valeria")
-            print(str(puntosCarlos) + "carlos")
-            continue
-        elif i not in LetrasCarlos and i in LetrasValeria:
-            puntosValeria = puntosValeria + 1
-            lista.append('V')
-            print(lista)
-            print(str(puntosValeria) + "valeria")
-            print(str(puntosCarlos) + "carlos")
-            continue
-        elif i in LetrasCarlos and i not in LetrasValeria:
+        if i in LetrasCarlos and not i in LetrasValeria:
             puntosCarlos = puntosCarlos + 1
-            lista.append('C')
-            print(lista)
-            print(str(puntosValeria) + "valeria")
-            print(str(puntosCarlos) + "carlos")
-            continue
-        elif i not in LetrasCarlos and i not in LetrasValeria:
+        if i in LetrasValeria and not i in LetrasCarlos:
             puntosValeria = puntosValeria + 1
-            lista.append('P')
-            print(lista)
-            print(str(puntosValeria) + "valeria")
-            print(str(puntosCarlos) + "carlos")
-            continue
+        if puntosCarlos == puntosValeria:
+            pekkedex.append('P')
+        elif puntosCarlos > puntosValeria:
+            pekkedex.append('C')
+        else :
+            pekkedex.append('V')
+    return(pekkedex)
 
 print(juegoPekkemon('AYS','LQK','YSGLZQUQSKRQSBVLVJGQ'))
